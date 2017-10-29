6b57d886f7a0fd3cf9352367e1ed27c10547a853
Andrii Lundiak <landike@gmail.com>
2017-10-29 21:17:25 +0100

    Added SSL/HTTPS/cert code changes

--- README.md
+++ README.md
@@ -22,6 +22,11 @@ NodeJS based Skype bot to create activity in chats
 ## Bot instance details
 - Default url/host for messages listening/broadcasting - `http://localhost:3978/api/messages`
 - `botbuilder` has built-in support for powerful AI frameworks such as LUIS.
+- Emulator works ok locally with HTTP server, but on Heroku it requires HTTPS.
+
+## SSL Self-Signed Certificate 
+- [Quick guide on how to set up a self-signed SSL certificate on Ubuntu](http://qugstart.com/blog/linux/quickest-way-to-create-a-self-signed-ssl-certificate-in-ubuntu/)
+- [Node.js and RESTify Server with both HTTP and HTTPS](http://qugstart.com/blog/node-js/node-js-restify-server-with-both-http-and-https/)
 
 
 ## Bot Related Services
@@ -30,6 +35,14 @@ NodeJS based Skype bot to create activity in chats
 - [Dev WebControl Generator](https://dev.skype.com/webcontrol)
 - [HTML code snippet builder](https://latest-swx.cdn.skype.com/lwc/sdk/0.0.835/index-builder.html)
 
+## Deployment to Heroku
+- Locally, to works with http, but on remote Heroku instance, it requires https, to be called from Skype API.
+- Do not hardcode port in nodejs server js file, use `process.env.PORT`, which is kinda required to use on Heroku, due to errors like:
+>[`Web process failed to bind to $PORT within 60 seconds of launch`](https://stackoverflow.com/questions/31092538/heroku-node-js-error-r10-boot-timeout-web-process-failed-to-bind-to-port-w)
+- If u use process.env.PORT, then Heroku https URL will NOT need append port value. Heroku applies SSL certificate from heroku.com.
+
+- [how to handle HTTP and HTTPS?](https://stackoverflow.com/questions/13186134/node-js-express-and-heroku-how-to-handle-http-and-https)
+- [HTTPS + SSL on Heroku - Node + Express](https://stackoverflow.com/questions/25148507/https-ssl-on-heroku-node-express)
 
 ## Azure based things
 - [Azure](https://portal.azure.com/)
--- app.js
+++ app.js
@@ -1,21 +1,57 @@
 //
 // Restify Server
 //
+var http = require('http');
+var https = require('https');
+
 var builder = require('botbuilder');
 var restify = require('restify');
+var fs = require('fs');
+
+var isProd = process.env.NODE_ENV === 'production';
+var hostName = isProd ? 'kanapka-bot.herokuapp.com' : 'localhost';
+
+// key: fs.readFileSync('./ssl_self_signed/server.key'),
+// key: fs.readFileSync('./var2/key.pem'),
+var keyFile = fs.readFileSync(`./ssl/${hostName}.key`);
+
+// certificate: fs.readFileSync('./ssl_self_signed/server.crt')
+// certificate: fs.readFileSync('./var2/ssl.crt')
+var certFile = fs.readFileSync(`./ssl/${hostName}.cert`);
 
 // var restifyServerPort = process.env.BOT_PORT || process.env.PORT || 3978;
+var restifyServerPort = process.env.PORT || 3978;
 // Using BOT_PORT (3978) doesn't work on Heroku. It throws error, when real communication starts:
 // => dyno= connect= service= status=503 bytes= protocol=https
 // => Web process failed to bind to $PORT within 60 seconds of launch
 // That is why, maybe better to use process.env.PORT, which is random per Heroku (on localhost it's random also)
-var restifyServerPort = process.env.PORT; 
+// var restifyHttpsServerPort = 8443; 
+var restifyHttpsServerPort = process.env.PORT || 8443;
+// var restifyHttpsServerPort = process.env.BOT_HTTPS_PORT || process.env.PORT || 8443;
 
-// Setup Restify Server
-var server = restify.createServer();
-server.listen(restifyServerPort, function () {
-   console.log('%s listening to %s', server.name, server.url); 
+// Setup HTTP Restify Server
+var server = restify.createServer({
+    name: 'skype-bot-http',
+    version: '1.0.0'
 });
+
+// var server = http.createServer({// doesn't work, due to errors
+//     name: 'skype-bot-http',
+//     version: '1.0.0'
+// });
+
+// Setup HTTPS Restify Server
+// var httpsServer = restify.createServer({
+//     name: 'skype-bot-https',
+//     version: '1.0.0',
+//     key: keyFile,
+//     certificate: certFile, // this way, server.url is HTTPS and HTTPS works good.
+//     _httpsServerOptions: {
+//         key: keyFile,
+//         cert: certFile // this way, server.url is HTTP and HTTPS server works in fact
+//         // certificate: certFile // this way, server.url is HTTP BUT HTTPS server doesn't work => ERR_SSL_VERSION_OR_CIPHER_MISMATCH
+//     }
+// });
 
 // Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
@@ -24,27 +60,54 @@ var connector = new builder.ChatConnector({
 });
 
 // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
-var bot = new builder.UniversalBot(connector, function (session) {
+var bot = new builder.UniversalBot(connector, function(session) {
+	// console.log(session);
     session.send("You said: %s", session.message.text);
 });
 
+bot.dialog('greetings', [
+    function (session) {
+        session.beginDialog('askName');
+    },
+    function (session, results) {
+        session.endDialog(`Hello ${results.response}!`);
+    }
+]);
+
+bot.dialog('askName', [
+    function (session) {
+        builder.Prompts.text(session, 'Hi! What is your name?');
+    },
+    function (session, results) {
+        session.endDialogWithResult(results);
+    }
+]);
+
+
 // Listen for messages from users 
 server.post('/api/messages', connector.listen());
+// httpsServer.post('/api/messages', connector.listen());
 
-// bot.dialog('greetings', [
-//     function (session) {
-//         session.beginDialog('askName');
-//     },
-//     function (session, results) {
-//         session.endDialog(`Hello ${results.response}!`);
-//     }
-// ]);
+var setup_server = function(app) {
+    function respond(req, res, next) {
+    	console.log('respond CALL', req.params);
+        res.send('I see you ' + req.params.name);
+    }
 
-// bot.dialog('askName', [
-//     function (session) {
-//         builder.Prompts.text(session, 'Hi! What is your name?');
-//     },
-//     function (session, results) {
-//         session.endDialogWithResult(results);
-//     }
-// ]);
+    console.log('setup_server CALL');
+    // Routes
+    app.get('/test/:name', respond);
+}
+setup_server(server);
+// setup_server(httpsServer);
+
+
+server.listen(restifyServerPort, function() {
+    // console.log(server);
+    console.log('%s listening to %s', server.name, server.url);
+});
+
+// httpsServer.listen(restifyHttpsServerPort, function() {
+//     // console.log(httpsServer);
+//     console.log('%s listening to %s', httpsServer.name, httpsServer.url);
+// });
